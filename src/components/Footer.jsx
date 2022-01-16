9366ad71dccbe5bd608550242c1025190f084bbe
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-16 22:42:09 +0100

    footer icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import {Col, Container, Row} from 'react-bootstrap'
 import {TiMail, TiSocialFacebookCircular, TiSocialLinkedinCircular, TiSocialGithubCircular} from "react-icons/ti";
+import {SiDiscord} from "react-icons/si"
+import {GiVibratingSmartphone} from 'react-icons/gi'
+
 
 const Footer =()=>{
     return (
@@ -11,6 +14,7 @@ const Footer =()=>{
                     <li >Via G. La Pira,23 70126-Bari Italia</li>
                     <li>P.Iva 05736870725 </li>
                     <li><TiMail style={{fontSize:"24px"}}/> francescomicunco@gmail.com</li>
+                    <li><GiVibratingSmartphone style={{fontSize:"24px"}}/>+39 350.189.1365</li>
                 </ul>
                 </Col>
                 <Col xs={12} md={4}><h3 style={{color:"black"}}>Link</h3>
@@ -20,7 +24,8 @@ const Footer =()=>{
                
             <TiSocialFacebookCircular style={{fontSize:"24px"}}/>
             <TiSocialLinkedinCircular style={{fontSize:"24px"}}/>
-            <TiSocialGithubCircular style={{fontSize:"24px"}}/></Col>
+            <TiSocialGithubCircular style={{fontSize:"24px"}}/>
+            <SiDiscord style={{fontSize:"24px"}}/></Col>
             </Row>
         </Container>
 </div>)
