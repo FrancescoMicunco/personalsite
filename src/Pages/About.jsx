87789955d063d9ca41b6879ec21576b6a81d6e06
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-25 22:37:53 +0100

    structure

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
-const About  = ()=>{
-     return (
-          <Container fluid>
-  <Row className="justify-content-center">
-      <Col xs={12} md={6}><h1>This is image section</h1></Col>
-      <Col xs={12} md={6}><h1>This is Tag section</h1></Col>
-  </Row>
-  
-          </Container>
-     
-      )
-     
+import { Container, Row, Col } from 'react-bootstrap'
+import Experience from '../components/experience'
+
+const About = () => {
+
+    return (
+        <Container fluid >
+            <Row className="justify-content-center">
+                <Col xs={12} md={6} style={{
+                    backgroundImage: "url('../processor.jpg')", backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat'
+                }}>
+
+                </Col>
+                <Col xs={12} md={6} className="py-md-5 px-md-5">
+                    <h2>About me</h2>
+                    <p>Ho una esperienza consulenziale pluriennale, sviluppata in contesti differenti, passando dalla finanza alle start-up. Questo mi consente di fornire un servizio di ampio respiro in ambito di sviluppo, che non si esaurisce nella mera, seppur efficace, scrittura del codice, ma si estende anche alla pianificazione strategica del progetto</p>
+
+                    <p>Sviluppo codice con le più moderne tecnologie, che rendono il progetto altamente efficace, veloce, e scalabile. Ogni componente viene testato e validato, per ridurre al massimo il rischio di eventuali malfunzionamenti.  </p>
+                    <div style={{ border: "solid 0.5px gray" }}></div>
+                    
+                        <Experience />
+
+
+                </Col>
+            </Row>
+
+        </Container>
+
+    )
+
 }
-     export default About
+export default About
---
+++ src/components/experience.jsx
@@ -0,0 +1,47 @@
+import {Col} from 'react-bootstrap'
+
+
+const Experience = () =>{
+    return (
+        <>
+        <Col xs={12} className="d-flex justify-content-around">
+                            <h3>Javascript</h3>
+                            <h3>80%</h3>
+                        </Col>
+                        <Col xs={12}>
+                            <div style={{ backgroundColor: "green" }}>
+                                <div style={{ border: "solid 10px yellow", width: "80%" }}></div>
+                            </div>
+                        </Col>
+                        <Col xs={12} className="d-flex justify-content-around">
+                            <h3>React</h3>
+                            <h3>80%</h3>
+                        </Col>
+                        <Col xs={12}>
+                            <div style={{ backgroundColor: "green" }}>
+                                <div style={{ border: "solid 10px yellow", width: "90%" }}></div>
+                            </div>
+                        </Col>
+                        <Col xs={12} className="d-flex justify-content-around">
+                            <h3>nodeJs</h3>
+                            <h3>80%</h3>
+                        </Col>
+                        <Col xs={12}>
+                            <div style={{ backgroundColor: "green" }}>
+                                <div style={{ border: "solid 10px yellow", width: "80%" }}></div>
+                            </div>
+                        </Col>
+                        <Col xs={12} className="d-flex justify-content-around">
+                            <h3>MongoDb & PostgreSQL</h3>
+                            <h3>70%</h3>
+                        </Col>
+                        <Col xs={12}>
+                            <div style={{ backgroundColor: "green" }}>
+                                <div style={{ border: "solid 10px yellow", width: "70%" }}></div>
+                            </div>
+                        </Col>
+</>
+    )
+}
+
+export default Experience
